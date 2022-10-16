a45e3fe63fb91b37e25751f24e340d7b243ef8f2
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-10-16 11:13:54 +0100

    height hit raycast
    raycast to detect height of obstacle

--- Assets/scripts/parkour-system/EnvironmentScanner.cs
+++ Assets/scripts/parkour-system/EnvironmentScanner.cs
@@ -5,6 +5,7 @@ public class EnvironmentScanner : MonoBehaviour
     [SerializeField] private Vector3 forwardRayOffset = new Vector3(0, .25f, 0);
     [SerializeField] private float forwardRayLength = 0.8f;
     [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private float heightRayLength = 5f;
     public ObstacleHitData ObstacleCheck()
     {
         ObstacleHitData hitData = new ObstacleHitData();
@@ -12,6 +13,14 @@ public class EnvironmentScanner : MonoBehaviour
         hitData.forwardHitFound = Physics.Raycast(forwardOrigin, transform.forward, out hitData.forwardHit,
             forwardRayLength, obstacleLayer);
         Debug.DrawRay(forwardOrigin, transform.forward * forwardRayLength, hitData.forwardHitFound ? Color.green : Color.red);
+
+        if (hitData.forwardHitFound)
+        {
+            Vector3 heightOrigin = hitData.forwardHit.point + Vector3.up * heightRayLength;
+            hitData.heightHitFound = Physics.Raycast(heightOrigin, Vector3.down, out hitData.heightHit, heightRayLength, obstacleLayer);
+            Debug.DrawRay(heightOrigin, Vector3.down * heightRayLength, hitData.heightHitFound ? Color.green : Color.red);
+        }
+
         return hitData;
     }
 }
@@ -19,5 +28,7 @@ public class EnvironmentScanner : MonoBehaviour
 public struct ObstacleHitData
 {
     public bool forwardHitFound;
+    public bool heightHitFound;
     public RaycastHit forwardHit;
+    public RaycastHit heightHit;
 }
